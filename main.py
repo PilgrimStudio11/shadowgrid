6fbdc9514f2189fcfee00597379ecc13a2e8b8b8
PilgrimStudio11 <as374638@gmail.com>
2026-01-12 09:35:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import os
-from flask import Flask, send_from_directory
+import json
+from flask import Flask, send_from_directory, request
+from flask_sock import Sock # Добавьте flask-sock в requirements.txt
 
 app = Flask(__name__, static_folder='.')
+sock = Sock(app)
+
+# В 2026 году мы храним ключ архитектора в памяти или простом файле
+ARCHITECT_PUB_KEY = None
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
+
+@sock.route('/ws/master')
+def master_socket(ws):
+    global ARCHITECT_PUB_KEY
+    while True:
+        data = ws.receive()
+        msg = json.loads(data)
+        
+        if msg['command'] == "HANDSHAKE":
+            if ARCHITECT_PUB_KEY is None:
+                ARCHITECT_PUB_KEY = msg['master_id']
+                ws.send(json.dumps({"status": "LINKED", "msg": "Вы установлены как Архитектор"}))
+            elif ARCHITECT_PUB_KEY == msg['master_id']:
+                ws.send(json.dumps({"status": "READY", "msg": "Связь подтверждена"}))
+            else:
+                ws.send(json.dumps({"status": "DENIED", "msg": "У Маяка уже есть владелец"}))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
